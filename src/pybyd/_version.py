521fcef40d4b4ba065e4268bb892943099a80747
Joel Kåberg <joel@kaberg.me>
2026-02-12 12:17:30 +0000

    use alternative apphroach

--- src/pybyd/_version.py
+++ src/pybyd/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.15.dev0+gb65bac96d.d20260212'
-__version_tuple__ = version_tuple = (0, 0, 15, 'dev0', 'gb65bac96d.d20260212')
+__version__ = version = '0.0.16.dev0+g3847dbd33.d20260212'
+__version_tuple__ = version_tuple = (0, 0, 16, 'dev0', 'g3847dbd33.d20260212')
 
 __commit_id__ = commit_id = None
